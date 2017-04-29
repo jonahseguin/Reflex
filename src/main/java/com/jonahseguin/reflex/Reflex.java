a4e4bda155195e547324b8e4d4ef92e473fe4055
jonahseguin <jonahseguin@icloud.com>
2017-04-28 21:18:19 -0600

    Remove remote authentication

--- src/main/java/com/jonahseguin/reflex/Reflex.java
+++ src/main/java/com/jonahseguin/reflex/Reflex.java
@@ -6,9 +6,6 @@ package com.jonahseguin.reflex;
 
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
-import com.jonahseguin.reflex.auth.AuthCaller;
-import com.jonahseguin.reflex.auth.AuthMe;
-import com.jonahseguin.reflex.auth.ShawXAuth;
 import com.jonahseguin.reflex.backend.command.RCommandHandler;
 import com.jonahseguin.reflex.backend.configuration.LanguageConfig;
 import com.jonahseguin.reflex.backend.configuration.RLang;
@@ -30,10 +27,10 @@ import com.jonahseguin.reflex.oldchecks.trigger.TriggerManager;
 import com.jonahseguin.reflex.player.reflex.ReflexCache;
 import com.jonahseguin.reflex.player.reflex.ReflexPlayer;
 import com.jonahseguin.reflex.util.obj.Lag;
-import com.jonahseguin.reflex.util.utility.ReflexException;
 import ninja.amp.ampmenus.MenuListener;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Set;
 
@@ -41,13 +38,12 @@ import java.util.Set;
  * Reflex - AntiCheat for Minecraft Servers
  * https://shawckz.com/product/reflex
  *
- * @author Jonah Seguin (Shawckz)
+ * @author Jonah Seguin
  * @version 2.0.0 (${project.version})
  * @since 1.0.0
  */
-public class Reflex extends AuthMe {
+public class Reflex extends JavaPlugin {
 
-    public static boolean couldStart = true;
     private static Reflex instance;
     private ReflexConfig reflexConfig;
     private ProtocolManager protocolManager;
@@ -62,7 +58,6 @@ public class Reflex extends AuthMe {
     private ReflexBanManager banManager;
     private RCommandHandler commandHandler;
     private AlertManager alertManager;
-    private boolean en = false;
 
     public static String getPrefix() {
         return RLang.format(ReflexLang.ALERT_PREFIX);
@@ -87,108 +82,90 @@ public class Reflex extends AuthMe {
 
     @Override
     public void onEnable() {
-        getLogger().info("[Start] Starting Reflex v" + getDescription().getVersion());
+        getLogger().info("[Start] Starting Reflex v" + getDescription().getVersion() + " by Jonah Seguin (Shawckz).");
         instance = this;
+        reflexConfig = new ReflexConfig(instance);
+        lang = new LanguageConfig(instance);
+        new DBManager(instance);
+        cache = new ReflexCache(instance);
 
-        getLogger().info("[Auth] Authenticating");
+        //Make reload-friendly, load all online players
+        for (Player pl : getOnlinePlayers()) {
+            ReflexPlayer cp = cache.loadReflexPlayer(pl.getName());
+            if (cp != null) {
+                cache.put(cp);
+            } else {
+                cp = cache.create(pl.getName(), pl.getUniqueId().toString());
+                cache.put(cp);
+                cp.update();
+            }
+        }
 
-        ShawXAuth.auth(this);
+        violationCache = new RDataCache();
+
+        autobanManager = new AutobanManager();
+        alertManager = new AlertManager(instance);
+
+        reflexTimer = new ReflexTimer(instance);
+
+        //Setup triggers, data captures, inspectors
+        triggerManager = new TriggerManager(instance);
+        triggerManager.setup();
+        dataCaptureManager = new DataCaptureManager(instance);
+        dataCaptureManager.setup();
+        inspectManager = new InspectManager(instance);
+        inspectManager.setup();
+
+        banManager = new ReflexBanManager();
+
+        //Commands
+        commandHandler = new RCommandHandler(instance);
+        commandHandler.registerCommands(new CmdReflex());
+        commandHandler.registerCommands(new CmdCancel());
+        commandHandler.registerCommands(new CmdInspect());
+        commandHandler.registerCommands(new CmdLookup());
+        commandHandler.registerCommands(new CmdBan());
+        commandHandler.registerCommands(new CmdSettings());
+        commandHandler.registerCommands(new CmdConfig());
+
+        getServer().getPluginManager().registerEvents(new BanListener(), instance);
+        getServer().getPluginManager().registerEvents(new BukkitListener(instance), instance);
+
+        MenuListener.getInstance().register(instance);
+
+        new PacketListener(instance);
+
+        Bukkit.getScheduler().runTaskTimer(instance, new Lag(), 1L, 1L);
+        getLogger().info("[Start] [Finish] Enabled Reflex v" + getDescription().getVersion() + " by Jonah Seguin (Shawckz).");
+
     }
 
     @Override
     public void onDisable() {
-        if (en) {
-            getLogger().info("[Stop] Disabling Reflex v" + getDescription().getVersion());
-            //Make reload-friendly, save all online players
-            int saved = 0;
-            for (Player pl : getOnlinePlayers()) {
-                cache.saveSync(pl);
-                saved++;
-            }
-            cache.clear();
-            getLogger().info("[Stop] Saved " + saved + " players.");
-
-            reflexTimer.clear();
-            reflexConfig.save();
-            reflexConfig = null;
-            commandHandler = null;
-            banManager = null;
-            inspectManager = null;
-            dataCaptureManager = null;
-            triggerManager = null;
-            reflexTimer = null;
-            lang = null;
-            cache = null;
-            instance = null;
-
-            getLogger().info("[Finish] Reflex v" + getDescription().getVersion() + " by Shawckz.");
+        getLogger().info("[Stop] Disabling Reflex v" + getDescription().getVersion());
+        //Make reload-friendly, save all online players
+        int saved = 0;
+        for (Player pl : getOnlinePlayers()) {
+            cache.saveSync(pl);
+            saved++;
         }
-    }
+        cache.clear();
+        getLogger().info("[Stop] Saved " + saved + " players.");
 
-    @Override
-    public final AuthCaller auth(ShawXAuth autheer) {
-        if (autheer == null) throw new ReflexException("Invalid autheer");
-        return () -> {
-            en = true;
-            reflexConfig = new ReflexConfig(instance);
-            lang = new LanguageConfig(instance);
-            new DBManager(instance);
-            cache = new ReflexCache(instance);
+        reflexTimer.clear();
+        reflexConfig.save();
+        reflexConfig = null;
+        commandHandler = null;
+        banManager = null;
+        inspectManager = null;
+        dataCaptureManager = null;
+        triggerManager = null;
+        reflexTimer = null;
+        lang = null;
+        cache = null;
+        instance = null;
 
-            //Make reload-friendly, load all online players
-            for (Player pl : getOnlinePlayers()) {
-                ReflexPlayer cp = cache.loadReflexPlayer(pl.getName());
-                if (cp != null) {
-                    cache.put(cp);
-                } else {
-                    cp = cache.create(pl.getName(), pl.getUniqueId().toString());
-                    cache.put(cp);
-                    cp.update();
-                }
-            }
-
-            violationCache = new RDataCache();
-
-            autobanManager = new AutobanManager();
-            alertManager = new AlertManager(instance);
-
-            reflexTimer = new ReflexTimer(instance);
-
-            //Setup triggers, data captures, inspectors
-            triggerManager = new TriggerManager(instance);
-            triggerManager.setup();
-            dataCaptureManager = new DataCaptureManager(instance);
-            dataCaptureManager.setup();
-            inspectManager = new InspectManager(instance);
-            inspectManager.setup();
-
-            banManager = new ReflexBanManager();
-
-            //Commands
-            commandHandler = new RCommandHandler(instance);
-            commandHandler.registerCommands(new CmdReflex());
-            commandHandler.registerCommands(new CmdCancel());
-            commandHandler.registerCommands(new CmdInspect());
-            commandHandler.registerCommands(new CmdLookup());
-            commandHandler.registerCommands(new CmdBan());
-            commandHandler.registerCommands(new CmdSettings());
-            commandHandler.registerCommands(new CmdConfig());
-
-            getServer().getPluginManager().registerEvents(new BanListener(), instance);
-            getServer().getPluginManager().registerEvents(new BukkitListener(instance), instance);
-
-            MenuListener.getInstance().register(instance);
-
-            new PacketListener(instance);
-
-            Bukkit.getScheduler().runTaskTimer(instance, new Lag(), 1L, 1L);
-            getLogger().info("[Finish] Reflex v" + getDescription().getVersion() + " by Shawckz.");
-        };
-    }
-
-    @Override
-    public final String authName() {
-        return "Reflex";
+        getLogger().info("[Stop] [Finish] Disabled Reflex v" + getDescription().getVersion() + " by Jonah Seguin (Shawckz).");
     }
 
     public RCommandHandler getCommandHandler() {
