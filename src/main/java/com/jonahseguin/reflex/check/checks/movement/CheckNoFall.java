986ead0a33ec754d2e340986efc9d34481f1a315
jonahseguin <jonahseguin@icloud.com>
2017-04-29 17:58:15 -0600

    Create NoFall check

--- src/main/java/com/jonahseguin/reflex/check/checks/movement/CheckNoFall.java
+++ src/main/java/com/jonahseguin/reflex/check/checks/movement/CheckNoFall.java
@@ -4,9 +4,64 @@
 
 package com.jonahseguin.reflex.check.checks.movement;
 
+import com.jonahseguin.reflex.Reflex;
+import com.jonahseguin.reflex.backend.configuration.annotations.ConfigData;
+import com.jonahseguin.reflex.check.Check;
+import com.jonahseguin.reflex.check.CheckType;
+import com.jonahseguin.reflex.player.reflex.ReflexPlayer;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerMoveEvent;
+
 /**
  * Created by Jonah Seguin on Sat 2017-04-29 at 16:45.
  * Project: Reflex
  */
-public class CheckNoFall {
+public class CheckNoFall extends Check {
+
+    @ConfigData("minimum-fall-distance")
+    private double minFallDistance = 3.0;
+
+    @ConfigData("minimum-attempts")
+    private int minAttempts = 3;
+
+    public CheckNoFall(Reflex instance) {
+        super(instance, CheckType.NO_FALL);
+    }
+
+    @EventHandler
+    public void onNoFallMove(final PlayerMoveEvent event) {
+        final Player player = event.getPlayer();
+        final ReflexPlayer rp = getPlayer(player);
+        if (canCheck(rp)) {
+            if (!rp.getData().isOnGround() && event.getTo().getY() < event.getFrom().getY()) {
+                // Falling downwards
+                rp.getData().fallDistance += (event.getFrom().getY() - event.getTo().getY());
+            }
+            if (rp.getData().fallDistance >= minFallDistance) {
+                if (rp.getData().isOnGround() || player.getFallDistance() == 0.0f) {
+                    rp.addAlertVL(getCheckType());
+                    if (rp.getAlertVL(getCheckType()) >= minAttempts) {
+                        // Fail & reset
+                        fail(rp, Math.round(rp.getData().fallDistance) + " fall");
+                        rp.setAlertVL(getCheckType(), 0);
+                        rp.getData().fallDistance = 0;
+                    }
+                } else {
+                    rp.setAlertVL(getCheckType(), 0);
+                }
+            }
+        }
+    }
+
+    private boolean canCheck(ReflexPlayer p) {
+        Player player = p.getBukkitPlayer();
+        if (player.getAllowFlight()) return false;
+        if (player.getGameMode() == GameMode.SPECTATOR || player.getGameMode() == GameMode.CREATIVE) return false;
+        if (player.isInsideVehicle()) return false;
+        if (player.getHealth() <= 0) return false;
+        return !p.getData().isInLiquid();
+    }
+
 }
