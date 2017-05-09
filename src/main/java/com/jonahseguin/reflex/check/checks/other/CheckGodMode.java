f16c0ca5701a057a2b96f303037bde431c61e53b
jonahseguin <jonahseguin@icloud.com>
2017-05-08 21:47:28 -0600

    Create GodMode check file

---
+++ src/main/java/com/jonahseguin/reflex/check/checks/other/CheckGodMode.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) Jonah Seguin (Shawckz) 2017.  You may not copy, re-sell, distribute, modify, or use any code contained in this document or file, collection of documents or files, or project.  Thank you.
+ */
+
+package com.jonahseguin.reflex.check.checks.other;
+
+/**
+ * Created by Jonah Seguin on Mon 2017-05-08 at 20:21.
+ * Project: Reflex
+ */
+public class CheckGodMode {
+
+    // TODO: Regulation of KeepAlive packets
+
+}
